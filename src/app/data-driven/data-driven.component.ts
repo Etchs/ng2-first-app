26d661af83e90fee67c9b700ed272dfa1ecf484a
Hesham <heshamusama@gmail.com>
2016-10-29 12:31:16 +0200

    dynamically create and add form elements to an exisitng form

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {FormGroup, FormControl, Validators, FormArray} from "@angular/forms";
 
 @Component({
   selector: 'app-data-driven',
@@ -15,8 +15,15 @@ export class DataDrivenComponent {
         'eml': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
       }),
       'psswd': new FormControl('', Validators.required),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([
+        new FormControl('Cooking', Validators.required)
+      ])
     });
+  }
+
+  onAddHobby() {
+    (<FormArray> this.myForm.controls.hobbies).push(new FormControl('', Validators.required));
   }
 
   onSubmit() {
